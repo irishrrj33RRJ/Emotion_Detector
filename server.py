617e862db0c9ed3390943c0c19d95ba3d33c3f4e
irishrrj33RRJ <irishrrj33@gmail.com>
2026-07-22 20:24:42 -0500

    Implement Flask app for emotion detection
    Add Flask routes for emotion detection and response formatting.

--- server.py
+++ server.py
@@ -1,2 +1,29 @@
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-emotion_detector("I am glad this happened")
+
+app = Flask('Emotion Detector')
+
+@app.route('/')
+def render_index_page():
+  return render_template('index.html')
+
+@app.route('/emotionDetector')
+def sent_detector():
+  text_to_analyze = request.args.get('textToAnalyze')
+  response = emotion_detector(text_to_analyze)
+  
+  if response['dominant_emotion'] is None:
+    return 'Invalid text! Please try again. '
+
+  return (
+    f"For the given statement, the system response is: "
+    f"'anger': {response['anger']}, "
+    f"'disgust': {response['disgust']}, "
+    f"'fear': {response['fear']}, "
+    f"'joy': {response['joy']}, "
+    f"'sadness': {response{'sadess']}, "
+    f"The dominant emotion is {response['dominant_emotion']}."
+  )
+
+if _name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 5000)
